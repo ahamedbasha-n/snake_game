c4e2b40080c9ec29f6f1673685c0b2532ded6e41
Ahamed Basha N <69695103+ahamedbasha-n@users.noreply.github.com>
2021-04-01 21:25:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -172,7 +172,7 @@ async function increaseScore(){
     presentScore.textContent=parseInt(presentScore.textContent)+1;
     let x = presentScore.textContent;
 
-    if (x==5){
+    if (x==1){
         console.log(x);
         document.getElementById("overlay").style.display = "block";
         vid.style.display = "block";
